594df286863b7a209b72237a018a080f79c6999d
KarthikRaj <rajkarthik711@gmail.com>
2019-11-04 18:20:58 -0500

    initial

---
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,198 @@
+package com.example.quizapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudSDK;
+import io.particle.android.sdk.cloud.ParticleDevice;
+import io.particle.android.sdk.cloud.ParticleEvent;
+import io.particle.android.sdk.cloud.ParticleEventHandler;
+import io.particle.android.sdk.cloud.exceptions.ParticleCloudException;
+import io.particle.android.sdk.utils.Async;
+
+public class MainActivity extends AppCompatActivity {
+    // MARK: Debug info
+    private final String TAG="QUIZ";
+
+    // MARK: Particle Account Info
+    private final String PARTICLE_USERNAME = "jenelleteaches@gmail.com";
+    private final String PARTICLE_PASSWORD = "nonsense1234";
+
+    // MARK: Particle device-specific info
+    private final String DEVICE_ID = "36001b001047363333343437";
+
+    // MARK: Particle Publish / Subscribe variables
+    private long subscriptionId;
+
+    // MARK: Particle device
+    private ParticleDevice mDevice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 1. Initialize your connection to the Particle API
+        ParticleCloudSDK.init(this.getApplicationContext());
+
+        // 2. Setup your device variable
+        getDeviceFromCloud();
+
+    }
+
+
+    /**
+     * Custom function to connect to the Particle Cloud and get the device
+     */
+    public void getDeviceFromCloud() {
+        // This function runs in the background
+        // It tries to connect to the Particle Cloud and get your device
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+
+            @Override
+            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                particleCloud.logIn(PARTICLE_USERNAME, PARTICLE_PASSWORD);
+                mDevice = particleCloud.getDevice(DEVICE_ID);
+                return -1;
+
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+
+                Log.d(TAG, "Successfully got device from Cloud");
+
+                // if you get the device, then go subscribe to events
+                subscribeToParticleEvents();
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                Log.d(TAG, exception.getBestMessage());
+            }
+        });
+    }
+
+
+
+    public void turnParticleGreen() {
+
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+            @Override
+            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                // put your logic here to talk to the particle
+                // --------------------------------------------
+                List<String> functionParameters = new ArrayList<String>();
+                functionParameters.add("green");
+                try {
+                    mDevice.callFunction("answer", functionParameters);
+
+                } catch (ParticleDevice.FunctionDoesNotExistException e1) {
+                    e1.printStackTrace();
+                }
+
+
+                return -1;
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+                // put your success message here
+                Log.d(TAG, "Success: Turned light green!!");
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                // put your error handling code here
+                Log.d(TAG, exception.getBestMessage());
+            }
+        });
+    }
+
+    public void turnParticleRed() {
+
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+            @Override
+            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                // put your logic here to talk to the particle
+                // --------------------------------------------
+                List<String> functionParameters = new ArrayList<String>();
+                functionParameters.add("red");
+                try {
+                    mDevice.callFunction("answer", functionParameters);
+
+                } catch (ParticleDevice.FunctionDoesNotExistException e1) {
+                    e1.printStackTrace();
+                }
+
+
+                return -1;
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+                // put your success message here
+                Log.d(TAG, "Success: Turned lights red!!");
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                // put your error handling code here
+                Log.d(TAG, exception.getBestMessage());
+            }
+        });
+
+
+
+    }
+
+
+
+
+    public void subscribeToParticleEvents() {
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+            @Override
+            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                subscriptionId = ParticleCloudSDK.getCloud().subscribeToAllEvents(
+                        "playerChoice",  // the first argument, "eventNamePrefix", is optional
+                        new ParticleEventHandler() {
+                            public void onEvent(String eventName, ParticleEvent event) {
+                                Log.i(TAG, "Received event with payload: " + event.dataPayload);
+                                String choice = event.dataPayload;
+                                if (choice.contentEquals("A")) {
+                                    turnParticleGreen();
+                                }
+                                else if (choice.contentEquals("B")) {
+                                    turnParticleRed();
+                                }
+
+                            }
+
+                            public void onEventError(Exception e) {
+                                Log.e(TAG, "Event error: ", e);
+                            }
+                        });
+
+
+                return -1;
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+                Log.d(TAG, "Success: Subscribed to events!");
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                Log.d(TAG, exception.getBestMessage());
+            }
+        });
+    }
+}
